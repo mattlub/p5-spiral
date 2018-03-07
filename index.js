c3fbd2f290ce00e4de5c3bb3ff9c7fbe7783ab14
matt lubel <matt_lubel@hotmail.com>
2018-03-08 00:35:59 +0100

    add ccapture

--- index.js
+++ index.js
@@ -1,36 +1,54 @@
-var dsxTween = new Tween(5, 105, 5000, 'quintInOut', 'loop')
-var dsyTween = new Tween(5, -4.5, 3000, 'quintInOut', 'loop')
+var dsxTween = new Tween(5, 105, 2000, 'quintInOut', 'loop')
+var dsyTween = new Tween(5, -4.5, 2000, 'quintInOut', 'loop')
 var rotateTween = new Tween(0, 2 * 3.14159265, 50000, 'linear', 'repeat')
 
+var CAPTURE_MAX_SECONDS = 8
+var canvas
+var capturer
+
+// Create a capturer that exports a WebM video
+if (CCapture) {
+  capturer = new CCapture({
+    format: 'webm',
+    framerate: 60,
+    verbose: true,
+    timeLimit: CAPTURE_MAX_SECONDS
+  })
+}
+
 function setup() { 
-  createCanvas(window.innerWidth, window.innerHeight);
+  createCanvas(window.innerWidth, window.innerHeight)
   spiral = new Spiral();
   var gui = new dat.GUI();
   gui.add(spiral, 'points', 0, 2000)
   gui.add(spiral, 'angle', 0, 20)
   gui.add(spiral, 'skipEvery', 0, 30).step(1) 
-  gui.add(spiral, 'offset', 0, 2000)
+  gui.add(spiral, 'offset', 0, 200)
   gui.add(spiral, 'zoom', 0, 20)
   gui.add(spiral, 'dotSizeChangeRate', 0, 0.5)
   gui.add(spiral, 'distChangeRate', 0, 1)
   gui.add(spiral, 'dotSizeX', 0, 20)
   gui.add(spiral, 'dotSizeY', 0, 20)
   gui.add(spiral, 'rotate')
+  gui.add(spiral, 'fadeIn')
+  gui.add(spiral, 'fadeOut')
   
-  gui.onChange = function (f) {
-    var i, j;
-    for (i in this.__controllers) this.__controllers[i].onChange (f);
-    for (i in this.__folders) for (j in this.__folders[i].__controllers) this.__folders[i].__controllers[j].onChange (f);
-  }
-  
-  // only redraw on change
+  // gui.onChange = function (f) {
+  //   var i, j;
+  //   for (i in this.__controllers) this.__controllers[i].onChange (f);
+  //   for (i in this.__folders) for (j in this.__folders[i].__controllers) this.__folders[i].__controllers[j].onChange (f);
+  // }
+  // // only redraw on change
   // noLoop()
   // gui.onChange(draw)
+
+  canvas = document.getElementById("defaultCanvas0")
 } 
 
 function draw() { 
   background(0)
   spiral.render()
+  if (capturer) capturer.capture(canvas)
 }
 
 function Spiral() {
@@ -45,6 +63,8 @@ function Spiral() {
   this.offset = 0
   this.skipEvery = 0
   this.rotate = false
+  this.fadeIn = true
+  this.fadeOut = true
   
   this.render = function() {
     push()
@@ -55,8 +75,12 @@ function Spiral() {
       if (this.skipEvery && i % this.skipEvery === 0) continue
       push();
       noStroke()
-      const opacity = 1 - ((i/this.points))
-      fill(`rgba(100, 100, 200, ${opacity})`)
+      let opacity = 1
+      if (this.fadeIn) opacity = opacity - (i < (100+this.offset) ? 1 - i/(100+this.offset) : 0)
+      if (this.fadeOut) opacity = opacity - ((i / this.points)) 
+
+      // fill(`rgba(100, 100, 200, ${opacity})`)
+      fill(`rgba(255, 255, 255, ${opacity})`)
       rotate(i * this.angle);
       const dist = Math.pow(i, this.distChangeRate) * this.zoom;
       const dotSizeX = dsxTween.getValue() * Math.pow(i, this.dotSizeChangeRate) * this.dotSizeX
